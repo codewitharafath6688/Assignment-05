4d57690d7afa47f9b59067954810270313202369
FiaruProject <111216829+FiaruProject@users.noreply.github.com>
2025-08-28 21:39:07 +0600

    heart button activated

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('ok');
+let heartIcon = document.getElementsByClassName('fa-heart')
+
+for(let heart of heartIcon){
+    heart.addEventListener('click', function(){
+        let heartCount = Number(document.getElementById('heart-count').innerText);
+        let finalCount = heartCount + 1;
+        document.getElementById('heart-count').innerText = finalCount; 
+    })
+}
+
